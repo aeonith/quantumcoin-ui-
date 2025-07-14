55f6b09db6cffa18b842b0ebf5d817f8d9267d9d
aeonith <aeonith@quantumcoincrypto.com>
2025-07-13 21:45:46 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendCoins() {
+  const log = document.getElementById("log");
+  log.innerHTML = "🔁 Sending coins...<br>✅ Transaction sent!";
+}
+
+function mineCoins() {
+  const log = document.getElementById("log");
+  log.innerHTML = "⛏️ Mining started...<br>💰 Block mined successfully! +50 QTC";
+}
