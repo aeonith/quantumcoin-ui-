f6dc740be341462033ea9a52dcad88819d7e4ddd
aeonith <aeonith@quantumcoincrypto.com>
2025-07-21 16:49:53 -0400

    Update blockchain.rs

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,46 +1,40 @@
-use serde::{Serialize, Deserialize};
-use sha2::{Sha256, Digest};
-use chrono::Utc;
+use crate::block::Block;
 use crate::transaction::Transaction;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Block {
-    pub index: u64,
-    pub previous_hash: String,
-    pub timestamp: i64,
-    pub transactions: Vec<Transaction>,
-    pub hash: String,
-}
-
-impl Block {
-    pub fn new(index: u64, previous_hash: String, transactions: Vec<Transaction>) -> Self {
-        let timestamp = Utc::now().timestamp();
-        let mut hasher = Sha256::new();
-        hasher.update(format!("{}{}{:?}{}", index, &previous_hash, &transactions, timestamp));
-        let hash = format!("{:x}", hasher.finalize());
-
-        Block { index, previous_hash, timestamp, transactions, hash }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct Blockchain {
     pub chain: Vec<Block>,
 }
 
 impl Blockchain {
     pub fn new() -> Self {
-        let genesis = Block::new(0, "0".into(), vec![]);
-        Blockchain { chain: vec![genesis] }
+        let mut blockchain = Blockchain { chain: vec![] };
+        blockchain.create_genesis_block();
+        blockchain
     }
 
-    pub fn add_block(&mut self, transactions: Vec<Transaction>) {
-        let prev = self.chain.last().unwrap();
-        let block = Block::new(prev.index + 1, prev.hash.clone(), transactions);
-        self.chain.push(block);
+    fn create_genesis_block(&mut self) {
+        let genesis_block = Block {
+            index: 0,
+            timestamp: current_timestamp(),
+            transactions: vec![],
+            previous_hash: "0".to_string(),
+            hash: "genesis_hash".to_string(),
+            nonce: 0,
+        };
+        self.chain.push(genesis_block);
     }
 
-    pub fn last_hash(&self) -> String {
-        self.chain.last().unwrap().hash.clone()
+    pub fn print_chain(&self) {
+        for block in &self.chain {
+            println!("{:#?}", block);
+        }
     }
+}
+
+fn current_timestamp() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis()
 }
