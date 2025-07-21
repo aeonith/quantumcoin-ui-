cedc605a1a9a4b71ff962940aa32235f649f2e79
aeonith <aeonith@quantumcoincrypto.com>
2025-07-20 20:18:43 -0400

    Update transaction.rs

--- src/transaction.rs
+++ src/transaction.rs
@@ -6,17 +6,25 @@ pub struct Transaction {
     pub recipient: String,
     pub amount: f64,
     pub signature: Option<String>,
-    pub timestamp: u128,
 }
 
 impl Transaction {
-    pub fn new(sender: String, recipient: String, amount: f64, signature: Option<String>, timestamp: u128) -> Self {
+    pub fn new(sender: String, recipient: String, amount: f64) -> Self {
         Transaction {
             sender,
             recipient,
             amount,
-            signature,
-            timestamp,
+            signature: None,
         }
+    }
+
+    pub fn set_signature(&mut self, signature: Vec<u8>) {
+        self.signature = Some(base64::encode(&signature));
+    }
+
+    pub fn get_signature_bytes(&self) -> Option<Vec<u8>> {
+        self.signature
+            .as_ref()
+            .and_then(|sig| base64::decode(sig).ok())
     }
 }
