b980555a28c2beecdb8cb9d13533c2b2a4f98493
aeonith <aeonith@quantumcoincrypto.com>
2025-07-22 15:35:06 -0400

    Update routes.rs

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,9 @@
-use actix_web::{web, HttpResponse, Responder};
-use crate::{blockchain::Blockchain, wallet::Wallet, transaction::Transaction};
-use serde::Deserialize;
-use std::sync::{Arc, Mutex};
+use actix_web::{web, HttpResponse};
+use crate::{blockchain::Blockchain, wallet::Wallet, transaction::Transaction, user::{User, load_user}};
+use crate::auth::{register, login};
+use std::sync::Mutex;
 
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct SendRequest {
     pub recipient: String,
     pub amount: u64,
@@ -11,58 +11,52 @@ pub struct SendRequest {
 }
 
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/wallet").route(web::get().to(get_wallet)))
-       .service(web::resource("/balance").route(web::get().to(get_balance)))
-       .service(web::resource("/mine").route(web::post().to(mine_block)))
-       .service(web::resource("/send").route(web::post().to(send_transaction)))
-       .service(web::resource("/info").route(web::get().to(get_address)));
+    cfg.service(web::resource("/wallet").route(web::get().to(wallet)))
+       .service(web::resource("/balance").route(web::get().to(balance)))
+       .service(web::resource("/mine").route(web::post().to(mine)))
+       .service(web::resource("/send").route(web::post().to(send_tx)))
+       .service(web::resource("/register").route(web::post().to(register)))
+       .service(web::resource("/login").route(web::post().to(login)));
 }
 
-async fn get_wallet(wallet: web::Data<Arc<Mutex<Wallet>>>) -> impl Responder {
+async fn wallet(wallet: web::Data<Mutex<Wallet>>) -> HttpResponse {
     let w = wallet.lock().unwrap();
     HttpResponse::Ok().json(&*w)
 }
 
-async fn get_balance(
-    blockchain: web::Data<Arc<Mutex<Blockchain>>>,
-    wallet: web::Data<Arc<Mutex<Wallet>>>,
-) -> impl Responder {
+async fn balance(
+    blockchain: web::Data<Mutex<Blockchain>>,
+    wallet: web::Data<Mutex<Wallet>>,
+) -> HttpResponse {
     let bc = blockchain.lock().unwrap();
     let w  = wallet.lock().unwrap();
-    let balance = bc.get_balance(&w.get_address());
-    HttpResponse::Ok().json(serde_json::json!({ "balance": balance }))
+    let b = bc.get_balance(&w.get_address());
+    HttpResponse::Ok().json(serde_json::json!({ "balance": b }))
 }
 
-async fn mine_block(
-    blockchain: web::Data<Arc<Mutex<Blockchain>>>,
-    wallet: web::Data<Arc<Mutex<Wallet>>>,
-) -> impl Responder {
+async fn mine(
+    blockchain: web::Data<Mutex<Blockchain>>,
+    wallet: web::Data<Mutex<Wallet>>,
+) -> HttpResponse {
     let mut bc = blockchain.lock().unwrap();
-    let w = wallet.lock().unwrap();
+    let w      = wallet.lock().unwrap();
     bc.mine_pending(&w.get_address());
-    bc.save();
-    HttpResponse::Ok().body("Block mined successfully")
+    HttpResponse::Ok().body("Mined a new block")
 }
 
-async fn send_transaction(
-    blockchain: web::Data<Arc<Mutex<Blockchain>>>,
-    wallet: web::Data<Arc<Mutex<Wallet>>>,
+async fn send_tx(
+    blockchain: web::Data<Mutex<Blockchain>>,
+    wallet: web::Data<Mutex<Wallet>>,
     req: web::Json<SendRequest>,
-) -> impl Responder {
+) -> HttpResponse {
     let mut bc = blockchain.lock().unwrap();
     let w = wallet.lock().unwrap();
 
     if !w.verify_password(&req.password) {
-        return HttpResponse::Unauthorized().body("Incorrect password");
+        return HttpResponse::Unauthorized().body("Bad password");
     }
 
     let tx = w.create_transaction(&req.recipient, req.amount);
     bc.add_transaction(tx);
-    bc.save();
-    HttpResponse::Ok().body("Transaction submitted successfully")
-}
-
-async fn get_address(wallet: web::Data<Arc<Mutex<Wallet>>>) -> impl Responder {
-    let w = wallet.lock().unwrap();
-    HttpResponse::Ok().json(w.get_address())
+    HttpResponse::Ok().json(serde_json::json!({ "status": "sent" }))
 }
