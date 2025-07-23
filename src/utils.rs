d59a88119c3de76e97d4451ced98f4d3b33575f8
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 15:15:01 -0400

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -2,8 +2,9 @@ use reqwest;
 use serde_json::Value;
 
 pub async fn get_btc_price_usd() -> Result<f64, reqwest::Error> {
-    let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+    let url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json";
     let response = reqwest::get(url).await?;
-    let json: Value = response.json().await?;
-    Ok(json["bitcoin"]["usd"].as_f64().unwrap_or(0.0))
+    let data: Value = response.json().await?;
+    let price = data["bpi"]["USD"]["rate_float"].as_f64().unwrap_or(0.0);
+    Ok(price)
 }
