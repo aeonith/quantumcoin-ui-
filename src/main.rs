c6e006a02df87568adeeddc97a49d3e4c11912af
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 11:39:40 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+mod blockchain;
+mod wallet;
+mod revstop;
+mod routes;
+mod p2p;
+
 use actix_cors::Cors;
 use actix_web::{web, App, HttpServer};
 use std::sync::{Arc, Mutex};
-
-mod blockchain;
-mod p2p;
-mod routes;
-mod wallet;
-
 use blockchain::Blockchain;
 use wallet::Wallet;
 use p2p::start_node;
@@ -14,25 +14,23 @@ use p2p::start_node;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Load blockchain and wallet
-    let blockchain = Arc::new(Mutex::new(Blockchain::load_or_create()));
-    let wallet = Arc::new(Mutex::new(Wallet::load_or_generate()));
+    let blockchain = Arc::new(Mutex::new(Blockchain::load_from_file().unwrap_or_else(|| {
+        Blockchain::new("tNzCy5NT+GORGIA+JCVIGAJUIBM...QNSATLVTHNBWXMZA783YP/ALNCM2GEAO1TZ==")
+    })));
+    let wallet = Arc::new(Mutex::new(Wallet::load_from_files()));
     let peers = Arc::new(Mutex::new(vec![]));
 
-    // Start P2P networking in separate thread
-    let p2p_peers = peers.clone();
+    // Start P2P networking
+    let chain_clone = blockchain.clone();
+    let peers_clone = peers.clone();
     std::thread::spawn(move || {
-        start_node(6000, p2p_peers);
+        start_node(6000, chain_clone, peers_clone);
     });
 
-    println!("🚀 QuantumCoin Node running at http://localhost:8080");
+    println!("✅ QuantumCoin Node running at http://localhost:8080");
 
-    // Start HTTP server
     HttpServer::new(move || {
-        let cors = Cors::default()
-            .allow_any_origin()
-            .allow_any_method()
-            .allow_any_header()
-            .max_age(3600);
+        let cors = Cors::default().allow_any_origin().allow_any_method().allow_any_header();
 
         App::new()
             .wrap(cors)
