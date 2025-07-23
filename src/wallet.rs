ba32893f5f2e638e9b5c0a7a7aba5116b84c75cc
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 15:16:43 -0400

    Update wallet.rs

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,9 +1,10 @@
-use pqcrypto_dilithium::dilithium2::{keypair, detached_sign, PublicKey, SecretKey};
-use pqcrypto_traits::sign::{PublicKey as TraitPublicKey, SecretKey as TraitSecretKey, Signature, Sign};
+use pqcrypto_dilithium::dilithium2::{
+    keypair, sign_detached, PublicKey, SecretKey, DetachedSignature,
+};
 use base64::{encode, decode};
-use std::fs::{File, create_dir_all};
-use std::io::{Read, Write};
 use serde::{Serialize, Deserialize};
+use std::fs::{self, File};
+use std::io::{Read, Write};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Wallet {
@@ -13,38 +14,43 @@ pub struct Wallet {
 
 impl Wallet {
     pub fn load_or_generate() -> Self {
-        if let Ok(mut file) = File::open("wallet_key.json") {
-            let mut data = String::new();
-            file.read_to_string(&mut data).unwrap();
-            serde_json::from_str(&data).unwrap()
-        } else {
-            let (pk, sk) = keypair();
-            let wallet = Wallet {
-                public_key: encode(pk.as_bytes()),
-                private_key: encode(sk.as_bytes()),
-            };
-            wallet.save_to_file();
-            wallet
+        if let Ok(data) = fs::read_to_string("wallet_key.json") {
+            if let Ok(wallet) = serde_json::from_str(&data) {
+                return wallet;
+            }
         }
+
+        let (pk, sk) = keypair();
+        let wallet = Wallet {
+            public_key: encode(pk.as_bytes()),
+            private_key: encode(sk.as_bytes()),
+        };
+
+        let json = serde_json::to_string_pretty(&wallet).unwrap();
+        fs::write("wallet_key.json", json).unwrap();
+        wallet
     }
 
-    fn save_to_file(&self) {
-        let serialized = serde_json::to_string_pretty(self).unwrap();
-        let _ = create_dir_all(".");
-        let mut file = File::create("wallet_key.json").unwrap();
-        file.write_all(serialized.as_bytes()).unwrap();
+    pub fn load_from_files() -> Option<Self> {
+        let data = fs::read_to_string("wallet_key.json").ok()?;
+        serde_json::from_str(&data).ok()
+    }
+
+    pub fn new() -> Self {
+        Self::load_or_generate()
     }
 
     pub fn sign_message(&self, message: &[u8]) -> Vec<u8> {
         let sk_bytes = decode(&self.private_key).unwrap();
-        let sk = TraitSecretKey::from_bytes(&sk_bytes).unwrap();
-        detached_sign(message, &sk).as_bytes().to_vec()
+        let sk = SecretKey::from_bytes(&sk_bytes).unwrap();
+        let sig = sign_detached(message, &sk);
+        sig.as_bytes().to_vec()
     }
 
-    pub fn verify_signature(&self, message: &[u8], sig: &[u8]) -> bool {
+    pub fn verify_signature(&self, message: &[u8], signature: &[u8]) -> bool {
         let pub_bytes = decode(&self.public_key).unwrap();
-        let sig = Signature::from_bytes(sig).unwrap();
-        let pk: PublicKey = TraitPublicKey::from_bytes(&pub_bytes).unwrap();
+        let sig = DetachedSignature::from_bytes(signature).unwrap();
+        let pk = PublicKey::from_bytes(&pub_bytes).unwrap();
         pk.verify_detached(&sig, message).is_ok()
     }
 
