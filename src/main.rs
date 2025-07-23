3b0d282e91fb26dec7cee444928cf8e803e0a2a2
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 15:02:43 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use actix_web::{web, App, HttpServer};
-use std::sync::{Arc, Mutex};
-
 mod wallet;
 mod blockchain;
-mod transaction;
 mod revstop;
 mod routes;
-mod btc;
-mod coingecko;
 
+use actix_web::{App, HttpServer};
+use std::sync::{Arc, Mutex};
 use wallet::Wallet;
 use blockchain::Blockchain;
 use revstop::RevStop;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("🚀 QuantumCoin Backend Starting...");
-
-    let wallet = Arc::new(Mutex::new(Wallet::load_or_create()));
-    let blockchain = Arc::new(Mutex::new(Blockchain::load_or_create(wallet.clone())));
-    let revstop = Arc::new(Mutex::new(RevStop::load_status()));
+    let wallet = Arc::new(Mutex::new(Wallet::load_from_files().unwrap_or_else(Wallet::new)));
+    let blockchain = Arc::new(Mutex::new(Blockchain::load_or_create()));
+    let revstop = Arc::new(Mutex::new(RevStop::status()));
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(wallet.clone()))
-            .app_data(web::Data::new(blockchain.clone()))
-            .app_data(web::Data::new(revstop.clone()))
+            .app_data(actix_web::web::Data::new(wallet.clone()))
+            .app_data(actix_web::web::Data::new(blockchain.clone()))
+            .app_data(actix_web::web::Data::new(revstop.clone()))
             .configure(routes::config)
     })
     .bind(("0.0.0.0", 8080))?
