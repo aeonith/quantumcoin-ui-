d9af4637758982272be29031be17b6d0dec9f3f1
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 13:17:54 -0400

    Update revstop.rs

--- src/revstop.rs
+++ src/revstop.rs
@@ -6,12 +6,10 @@ lazy_static! {
     static ref LOCKED: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
 }
 
-/// Is RevStop (lock) active for this address?
 pub fn is_revstop_active(addr: &str) -> bool {
     LOCKED.lock().unwrap().contains(addr)
 }
 
-/// Human-readable RevStop status
 pub fn get_revstop_status(addr: &str) -> String {
     if is_revstop_active(addr) {
         "🔒 ACTIVE".into()
@@ -20,12 +18,10 @@ pub fn get_revstop_status(addr: &str) -> String {
     }
 }
 
-/// Lock address (disable spending)
 pub fn lock_address(addr: &str) {
     LOCKED.lock().unwrap().insert(addr.to_string());
 }
 
-/// Unlock address
 pub fn unlock_address(addr: &str) {
     LOCKED.lock().unwrap().remove(addr);
 }
