6b388d3d8a877586b320f85df435047636cf044e
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 14:50:46 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,34 @@
-#![allow(unused)]
-#[macro_use] extern crate rocket;
-
+use actix_web::{web, App, HttpServer};
 use std::sync::{Arc, Mutex};
-use rocket::{Rocket, Build};
-use crate::wallet::Wallet;
-use crate::revstop::RevStop;
-use crate::blockchain::Blockchain;
-use crate::p2p::start_node;
 
 mod wallet;
-mod revstop;
 mod blockchain;
 mod transaction;
-mod block;
+mod revstop;
 mod routes;
-mod p2p;
 mod btc;
 mod coingecko;
 
-#[launch]
-fn rocket() -> Rocket<Build> {
-    let wallet = Arc::new(Mutex::new(Wallet::load_or_generate()));
-    let revstop = Arc::new(Mutex::new(RevStop::status()));
-    let blockchain = Arc::new(Mutex::new(Blockchain::new(wallet.clone())));
+use wallet::Wallet;
+use blockchain::Blockchain;
+use revstop::RevStop;
 
-    let peers = Arc::new(Mutex::new(vec![]));
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("🚀 QuantumCoin Backend Starting...");
 
-    // Start the P2P node in a separate thread
-    {
-        let blockchain_clone = blockchain.clone();
-        let peers_clone = peers.clone();
-        std::thread::spawn(move || {
-            start_node(6000, blockchain_clone, peers_clone);
-        });
-    }
+    let wallet = Arc::new(Mutex::new(Wallet::load_or_create()));
+    let blockchain = Arc::new(Mutex::new(Blockchain::load_or_create(wallet.clone())));
+    let revstop = Arc::new(Mutex::new(RevStop::load_status()));
 
-    rocket::build()
-        .manage(wallet)
-        .manage(revstop)
-        .manage(blockchain)
-        .mount("/", routes![
-            routes::get_balance,
-            routes::send_coins,
-            routes::mine,
-            routes::revstop_status,
-            routes::revstop_enable,
-            routes::revstop_disable,
-            routes::last_transactions,
-            routes::export_wallet,
-            routes::get_address,
-            routes::buy_qtc
-        ])
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(wallet.clone()))
+            .app_data(web::Data::new(blockchain.clone()))
+            .app_data(web::Data::new(revstop.clone()))
+            .configure(routes::config)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
