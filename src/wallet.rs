304e3c62e568fe67c753591852e0e2ab0143f603
aeonith <aeonith@quantumcoincrypto.com>
2025-07-23 15:46:37 -0400

    Update wallet.rs

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,11 +1,13 @@
 use pqcrypto_dilithium::dilithium2::{
-    keypair, sign, verify_detached, PublicKey, SecretKey, DetachedSignature, SignedMessage,
+    keypair, sign, PublicKey, SecretKey, DetachedSignature,
 };
-use pqcrypto_traits::sign::{DetachedSignature as DetachedSigTrait, SignedMessage as SignedMsgTrait};
+use pqcrypto_traits::sign::{
+    SecretKey as SecretKeyTrait, PublicKey as PublicKeyTrait,
+    DetachedSignature as DetachedSigTrait,
+};
 use base64::{encode, decode};
-use std::fs::{File, OpenOptions};
+use std::fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 pub struct Wallet {
@@ -25,7 +27,6 @@ impl Wallet {
     pub fn save_to_files(&self, pub_path: &str, priv_path: &str) {
         let mut pub_file = File::create(pub_path).unwrap();
         let mut priv_file = File::create(priv_path).unwrap();
-
         pub_file.write_all(self.public_key.as_bytes()).unwrap();
         priv_file.write_all(self.private_key.as_bytes()).unwrap();
     }
@@ -33,13 +34,10 @@ impl Wallet {
     pub fn load_from_files(pub_path: &str, priv_path: &str) -> Self {
         let mut pub_file = File::open(pub_path).unwrap();
         let mut priv_file = File::open(priv_path).unwrap();
-
         let mut pub_key = String::new();
         let mut priv_key = String::new();
-
         pub_file.read_to_string(&mut pub_key).unwrap();
         priv_file.read_to_string(&mut priv_key).unwrap();
-
         Wallet {
             public_key: pub_key,
             private_key: priv_key,
@@ -49,8 +47,8 @@ impl Wallet {
     pub fn sign_message(&self, message: &[u8]) -> Vec<u8> {
         let sk_bytes = decode(&self.private_key).unwrap();
         let sk = SecretKey::from_bytes(&sk_bytes).unwrap();
-        let sig = sign(message, &sk);
-        sig.as_bytes().to_vec()
+        let signed = sign(message, &sk);
+        signed.as_bytes().to_vec()
     }
 
     pub fn verify_signature(&self, message: &[u8], signature: &[u8]) -> bool {
@@ -58,7 +56,7 @@ impl Wallet {
         let pk = PublicKey::from_bytes(&pk_bytes).unwrap();
 
         match DetachedSignature::from_bytes(signature) {
-            Ok(sig) => verify_detached(&sig, message, &pk).is_ok(),
+            Ok(sig) => pk.verify_detached(&sig, message).is_ok(),
             Err(_) => false,
         }
     }
