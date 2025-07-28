07986e65213f2f2af0c26f318798b3b93373b913
aeonith <aeonith@quantumcoincrypto.com>
2025-07-28 13:38:30 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,26 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+mod block;
+mod blockchain;
+mod transaction;
+mod wallet;
+mod revstop;
+mod routes;
+
+use blockchain::Blockchain;
+use rocket::tokio::sync::Mutex;
+use std::sync::Arc;
 
 #[macro_use]
 extern crate rocket;
 
-mod blockchain;
-mod routes;
-mod wallet;
+#[launch]
+fn rocket() -> _ {
+    let blockchain = Arc::new(Mutex::new(Blockchain::new()));
+    let wallet = Arc::new(Mutex::new(wallet::Wallet::new()));
+    let revstop = Arc::new(Mutex::new(revstop::RevStop::new()));
 
-use blockchain::Blockchain;
-use routes::{get_balance, get_wallet_keys, mine_block, send_transaction};
-use std::sync::Mutex;
-use wallet::Wallet;
-
-fn main() {
-    // Load or create wallet
-    let wallet = Wallet::load_from_files("wallet_public.key", "wallet_private.key")
-        .unwrap_or_else(|_| {
-            let new_wallet = Wallet::new();
-            new_wallet.save_to_files("wallet_public.key", "wallet_private.key").unwrap();
-            new_wallet
-        });
-
-    // Load or create blockchain
-    let blockchain = Blockchain::load_from_disk("blockchain.json")
-        .unwrap_or_else(|| Blockchain::new());
-
-    // Launch Rocket server
-    rocket::ignite()
-        .manage(Mutex::new(wallet))
-        .manage(Mutex::new(blockchain))
-        .mount(
-            "/",
-            routes![get_wallet_keys, send_transaction, mine_block, get_balance],
-        )
-        .launch();
+    rocket::build()
+        .manage(blockchain)
+        .manage(wallet)
+        .manage(revstop)
+        .mount("/", routes::routes())
 }
