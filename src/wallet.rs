019aff7b36fa6303674817ba59b5ff5efaa22401
aeonith <aeonith@quantumcoincrypto.com>
2025-07-28 13:11:35 -0400

    Update wallet.rs

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,8 +1,9 @@
-use pqcrypto_dilithium::dilithium2::{keypair, sign, DetachedSignature, PublicKey, SecretKey};
-use pqcrypto_traits::sign::{DetachedSignature as _, PublicKey as _, SecretKey as _};
-use base64::{engine::general_purpose, Engine as _};
-use std::fs::{self, File};
+use pqcrypto_dilithium::dilithium2::{keypair, sign_detached, DetachedSignature, PublicKey, SecretKey};
+use pqcrypto_traits::sign::{Signer, Verifier, Signature};
+use std::fs::{File};
 use std::io::{Read, Write};
+use std::path::Path;
+use base64::{encode, decode};
 
 pub struct Wallet {
     pub public_key: PublicKey,
@@ -10,65 +11,52 @@ pub struct Wallet {
 }
 
 impl Wallet {
-    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn generate() -> Self {
         let (pk, sk) = keypair();
-        Ok(Wallet {
-            public_key: pk,
-            secret_key: sk,
-        })
+        Wallet { public_key: pk, secret_key: sk }
+    }
+
+    pub fn save_to_files(&self, pub_path: &str, sec_path: &str) {
+        let mut pub_file = File::create(pub_path).expect("Failed to create pub key file");
+        pub_file.write_all(&self.public_key.as_bytes()).expect("Failed to write pub key");
+
+        let mut sec_file = File::create(sec_path).expect("Failed to create sec key file");
+        sec_file.write_all(&self.secret_key.as_bytes()).expect("Failed to write sec key");
+    }
+
+    pub fn load_from_files(pub_path: &str, sec_path: &str) -> Self {
+        let mut pub_bytes = Vec::new();
+        File::open(pub_path).unwrap().read_to_end(&mut pub_bytes).unwrap();
+        let public_key = PublicKey::from_bytes(&pub_bytes).unwrap();
+
+        let mut sec_bytes = Vec::new();
+        File::open(sec_path).unwrap().read_to_end(&mut sec_bytes).unwrap();
+        let secret_key = SecretKey::from_bytes(&sec_bytes).unwrap();
+
+        Wallet { public_key, secret_key }
     }
 
     pub fn get_address_base64(&self) -> String {
-        general_purpose::STANDARD.encode(self.public_key.as_bytes())
+        encode(self.public_key.as_bytes())
     }
 
     pub fn get_public_key_base64(&self) -> String {
-        general_purpose::STANDARD.encode(self.public_key.as_bytes())
+        encode(self.public_key.as_bytes())
     }
 
     pub fn get_private_key_base64(&self) -> String {
-        general_purpose::STANDARD.encode(self.secret_key.as_bytes())
+        encode(self.secret_key.as_bytes())
     }
 
-    pub fn sign_message(&self, message: &[u8]) -> Vec<u8> {
-        let sig = sign::detached_sign(message, &self.secret_key);
-        sig.as_bytes().to_vec()
+    pub fn sign_message(&self, message: &[u8]) -> DetachedSignature {
+        sign_detached(message, &self.secret_key)
     }
 
-    pub fn verify_message(&self, message: &[u8], signature: &[u8]) -> bool {
-        if let Ok(sig) = <DetachedSignature as pqcrypto_traits::sign::DetachedSignature>::from_bytes(signature) {
-            pqcrypto_dilithium::dilithium2::verify_detached(&sig, message, &self.public_key).is_ok()
+    pub fn verify_signature(&self, message: &[u8], signature: &[u8]) -> bool {
+        if let Ok(sig) = DetachedSignature::from_bytes(signature) {
+            sig.verify_detached(message, &self.public_key).is_ok()
         } else {
             false
         }
-    }
-
-    pub fn save_to_files(
-        &self,
-        pub_path: &str,
-        priv_path: &str,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        fs::write(pub_path, self.public_key.as_bytes())?;
-        fs::write(priv_path, self.secret_key.as_bytes())?;
-        Ok(())
-    }
-
-    pub fn load_from_files(
-        pub_path: &str,
-        priv_path: &str,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut pub_bytes = Vec::new();
-        let mut priv_bytes = Vec::new();
-
-        File::open(pub_path)?.read_to_end(&mut pub_bytes)?;
-        File::open(priv_path)?.read_to_end(&mut priv_bytes)?;
-
-        let pub_key = PublicKey::from_bytes(&pub_bytes)?;
-        let priv_key = SecretKey::from_bytes(&priv_bytes)?;
-
-        Ok(Wallet {
-            public_key: pub_key,
-            secret_key: priv_key,
-        })
     }
 }
